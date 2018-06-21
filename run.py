d796a8089cc818dd0839dd10361f698b72c1e678
wuling31715 <wuling31715@gmail.com>
2018-06-21 18:27:38 +0800

    a

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
+import datetime, random
+
+port = random.randrange(1000, 9999)
 
 app = Flask(__name__)
 
@@ -6,5 +9,10 @@ app = Flask(__name__)
 def index():
     return "<h1>Hello Word</h1>"
 
+@app.route("/test")
+def test():
+    return render_template('index.html')
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=port)
