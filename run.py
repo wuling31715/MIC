89885e41cb4bc6d66faa8be2fd1ce2a52218244b
wuling31715 <wuling31715@gmail.com>
2018-06-22 01:41:13 +0800

    a

--- run.py
+++ run.py
@@ -2,22 +2,20 @@ from flask import Flask, render_template
 from data.read import Reader
 import datetime, random
 
-
-
-port = random.randrange(1000, 9999)
 app = Flask(__name__)
+port = random.randrange(1000, 9999)
+today = datetime.datetime.now().strftime("%Y/%m/%d")
+
+def get_date(date):
+    return '%s/%s/%s' % (date[:4], date[4:6], date[6:8])
 
 @app.route("/")
 def index():
-    return "<h1>Hello Word</h1>"
-
-@app.route("/test")
-def test():
     rank_list = Reader.get_rank_list()
-    rank_today_list = max(rank_list)
-    print(rank_today_list)
+    rank_today_list = max(rank_list)    
     return render_template(
         'index.html', 
+        today = today,
         rank_today_list = rank_today_list[:101],
         )
 
