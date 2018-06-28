323edadfb40ec62e42763ec7d6bf6ce5f6afed2c
wuling31715 <wuling31715@gmail.com>
2018-06-29 05:21:57 +0800

    a

--- static/chart.js
+++ static/chart.js
@@ -1,7 +1,3 @@
-function top100_query(id) {
-    document.getElementById(id).click();
-};
-
 function query(id) {
     document.getElementById(id).click();
 };
@@ -16,7 +12,9 @@ function dropdown_select(id, content) {
 
 var scroll = new SmoothScroll('a[href*="#"]');
 
-new Chart(document.getElementById("top100_chart"), {
+
+var ctx = document.getElementById("top100_chart");
+var myChart = new Chart(ctx, {
     type: 'horizontalBar',
     data: {
         labels: company_name_array,
@@ -30,148 +28,11 @@ new Chart(document.getElementById("top100_chart"), {
         responsive: true,
         maintainAspectRatio: true,
         legend: {
-            display: false
+            display: false,
         },
         title: {
             display: true,
             text: '',
         },
-    }
-});
-
-var ctx = document.getElementById("bar_chart");
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: 'Hello World!',
-            data: [50, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
-var ctx = document.getElementById("line_chart");
-var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-                label: '# of Votes',
-                data: [50, 19, 35, 54, 23, 30],
-                fill: false,
-                backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                borderColor: 'rgba(255, 99, 132, 0.2)',
-            },
-            {
-                label: '# of Votes',
-                data: [20, 49, 35, 44, 73, 201],
-                fill: false,
-                backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                borderColor: 'rgba(255, 99, 132, 0.2)',
-            },
-        ]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
-
-var ctx = document.getElementById("pie_chart");
-var myChart = new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# of Votes',
-            data: [50, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
-new Chart(document.getElementById("radar_chart"), {
-    type: 'radar',
-    data: {
-        labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
-        datasets: [{
-            label: "1950",
-            fill: true,
-            backgroundColor: "rgba(179,181,198,0.2)",
-            borderColor: "rgba(179,181,198,1)",
-            pointBorderColor: "#fff",
-            pointBackgroundColor: "rgba(179,181,198,1)",
-            data: [8.77, 55.61, 21.69, 6.62, 6.82]
-        }, {
-            label: "2050",
-            fill: true,
-            backgroundColor: "rgba(255,99,132,0.2)",
-            borderColor: "rgba(255,99,132,1)",
-            pointBorderColor: "#fff",
-            pointBackgroundColor: "rgba(255,99,132,1)",
-            pointBorderColor: "#fff",
-            data: [25.48, 54.16, 7.61, 8.06, 4.45]
-        }]
-    },
-    options: {
-        title: {
-            display: true,
-            text: 'Distribution in % of world population'
-        }
     }
 });
