09f01f278ff4e01422744a105a99a5b889b5e171
wuling31715 <wuling31715@gmail.com>
2018-07-05 07:59:27 +0800

    a

--- run.py
+++ run.py
@@ -2,20 +2,20 @@ from flask import Flask, render_template, redirect, request
 from data.read import Reader
 import datetime, random
 
-def get_date(date):
-    return '%s/%s/%s' % (date[:4], date[4:6], date[6:8])
-
 app = Flask(__name__)
 port = random.randrange(1000, 9999)
 all_rank_list = Reader.get_rank_list()[::-1]
-date_max = get_date(str(all_rank_list[0][0]))
-today = datetime.datetime.now().strftime("%Y/%m/%d")
-industry_list = ['全部', '資訊硬體', '電商', '網路服務', '軟體', '半導體', '電信', '通訊設備', '材料', '工業', '電子', '機電設備', '資訊服務', '儲存']
-date_list = list()
 
-for i, j in enumerate(all_rank_list):
-    date_list.append(get_date(str(j[0])))
-    all_rank_list[i][0] = get_date(str(j[0]))    
+def get_date(date):
+    return '%s/%s/%s' % (date[:4], date[4:6], date[6:8])
+
+def get_date_list():
+    date_list = list()
+    for i, j in enumerate(all_rank_list):
+        date_list.append(get_date(str(j[0])))
+        all_rank_list[i][0] = get_date(str(j[0]))    
+    return date_list
+
 
 def get_rank_list(date, industry):
     rank_list = list()
@@ -33,7 +33,7 @@ def get_rank_list(date, industry):
                         rank_list.append(j)
         return rank_list[:200]
 
-def get_top100_rank_list(date, industry):
+def get_market_cap_list(date, industry):
     company_name_list = list()
     market_cap_list = list()
     if industry == '全部':
@@ -52,12 +52,28 @@ def get_top100_rank_list(date, industry):
                         market_cap_list.append(j[-1])
         return company_name_list[:100], market_cap_list[:100]
 
+def get_industry_market_cap_list():
+    industry_market_cap_list = list()
+    for i in industry_list:
+        tmp = list()
+        for j in date_list[1:]:
+            tmp.append(sum(get_market_cap_list(j, i)[1]))
+        industry_market_cap_list.append(tmp)        
+    return industry_market_cap_list
+
+
+date_max = get_date(str(all_rank_list[0][0]))
+today = datetime.datetime.now().strftime("%Y/%m/%d")
+industry_list = ['全部', '資訊硬體', '電商', '網路服務', '軟體', '半導體', '電信', '通訊設備', '材料', '工業', '電子', '機電設備', '資訊服務', '儲存']
+date_list = get_date_list()
+
 @app.route("/")
 def index():
     date = date_max
     industry = '全部'
     rank_list = get_rank_list(date, industry)
-    company_name_list, market_cap_list = get_top100_rank_list(date, industry)
+    company_name_list, market_cap_list = get_market_cap_list(date, industry)
+    industry_market_cap_list = get_industry_market_cap_list()
     return render_template(
         'index.html', 
         date = date,
@@ -67,6 +83,7 @@ def index():
         industry_list = industry_list,
         company_name_list = company_name_list,
         market_cap_list = market_cap_list,
+        industry_market_cap_list = industry_market_cap_list,
         )
 
 @app.route("/query", methods=['POST', 'GET'])
@@ -75,7 +92,8 @@ def query():
     industry = request.args.get('industry')
     anchor = request.args.get('anchor')
     rank_list = get_rank_list(date, industry)
-    company_name_list, market_cap_list = get_top100_rank_list(date, industry)
+    company_name_list, market_cap_list = get_market_cap_list(date, '全部')
+    industry_market_cap_list = get_industry_market_cap_list()
     return render_template(
         'index.html', 
         anchor = anchor,
@@ -86,6 +104,7 @@ def query():
         industry_list = industry_list,
         company_name_list = company_name_list,
         market_cap_list = market_cap_list,
+        industry_market_cap_list = industry_market_cap_list,
         )
 
 if __name__ == "__main__":
--- static/chart.js
+++ static/chart.js
@@ -12,14 +12,12 @@ function dropdown_select(id, content) {
 
 var scroll = new SmoothScroll('a[href*="#"]');
 
-
-var ctx = document.getElementById("top100_chart");
-var myChart = new Chart(ctx, {
+var rank_market_cap_chart = new Chart(document.getElementById("rank_market_cap_chart"), {
     type: 'horizontalBar',
     data: {
         labels: company_name_array,
         datasets: [{
-            label: "",
+            label: "市值",
             data: market_cap_array,
             backgroundColor: 'rgba(255, 99, 132, 0.2)',
         }]
@@ -28,7 +26,89 @@ var myChart = new Chart(ctx, {
         responsive: true,
         maintainAspectRatio: true,
         legend: {
-            display: false,
+            display: true,
+        },
+        title: {
+            display: true,
+            text: '',
+        },
+    }
+});
+
+var rank_industry_chart = new Chart(document.getElementById("rank_industry_chart"), {
+    type: 'bar',
+    data: {
+        labels: date_array,
+        datasets: [{
+            label: "資訊硬體",
+            data: industry0_array,
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        },{
+            label: "電商",
+            data: industry1_array,
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+        },{
+            label: "網路服務",
+            data: industry2_array,
+            backgroundColor: 'rgba(255, 206, 86, 0.2)',
+        },{
+            label: "軟體",
+            data: industry3_array,
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        },{
+            label: "半導體",
+            data: industry4_array,
+            backgroundColor: 'rgba(153, 102, 255, 0.2)',
+        },{
+            label: "電信",
+            data: industry5_array,
+            backgroundColor: 'rgba(255, 159, 64, 0.2)',
+        },{
+            label: "通訊設備",
+            data: industry6_array,
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        },{
+            label: "材料",
+            data: industry7_array,
+            backgroundColor:  'rgba(54, 162, 235, 0.2)',
+        },{
+            label: "工業",
+            data: industry8_array,
+            backgroundColor: 'rgba(255, 206, 86, 0.2)',
+        },{
+            label: "電子",
+            data: industry9_array,
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        },{
+            label: "機電設備",
+            data: industry10_array,
+            backgroundColor: 'rgba(153, 102, 255, 0.2)',
+        },{
+            label: "資訊服務",
+            data: industry11_array,
+            backgroundColor: 'rgba(255, 159, 64, 0.2)'
+        },{
+            label: "儲存",
+            data: industry12_array,
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        },
+    ],
+    },
+    options: {
+        responsive: true,
+        legend: {
+            display: true,
+        },
+        scales: {
+            xAxes: [{
+               stacked: true,
+            }],
+            yAxes: [{
+               stacked: true,
+               ticks: {
+                max: 4000000000000000,
+            }
+            }]
         },
         title: {
             display: true,
